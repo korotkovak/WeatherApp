c3a335dac623d544e2b746145d3d219d0de2a072
Kristina Korotkova <k.korotkova@shinobi.software>
2024-01-24 12:50:23 +0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kristina Korotkova on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
